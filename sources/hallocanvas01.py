de9d3026d254cc96d8fbab81289be3a12674dbb7
Jörg Kantel <joerg@kantel.de>
2017-06-27 20:58:47 +0200

    Hallo Canvas

---
+++ sources/hallocanvas01.py
@@ -0,0 +1,17 @@
+# Hallo Canvas!
+
+import tkinter as tk
+
+cw = 400        # Canvas width
+ch = 100        # Canvas height
+
+root = tk.Tk()
+root.title("Hallo Welt!")
+
+my_canvas = tk.Canvas(root, width = cw, height = ch, background = "white")
+my_canvas.grid(column = 0, row = 0)
+
+xy = cw//2, ch//2
+my_canvas.create_text(xy, text = "Hallo Jörg!")
+
+root.mainloop()
