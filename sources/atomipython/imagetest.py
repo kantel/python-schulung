6d0f13d6f63b798e69745ced4f1bcf155666e0f0
Jörg Kantel <joerg@kantel.de>
2017-09-13 16:53:05 +0200

    Atom Test

--- sources/atomipython/imagetest.py
+++ sources/atomipython/imagetest.py
@@ -2,6 +2,8 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
+%pylab inline
+
 me = Image.open("/Users/kantel/git/python-schulung/sources/atomipython/images/jkantel2.jpg")
 arr = np.array(me.getdata(), np.uint8).reshape(me.size[1], me.size[0], 3)
 
