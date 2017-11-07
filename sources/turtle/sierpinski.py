1502145eba717d61c6d14fea04006d3f3f4d8622
Jörg Kantel <joerg@kantel.de>
2017-11-07 15:55:12 +0100

    Sierpinski-Dreieck publiziert

--- sources/turtle/sierpinski.py
+++ sources/turtle/sierpinski.py
@@ -1,4 +1,4 @@
-# Sierpinksy-Dreieck mit der Turtlegraphik
+# Sierpinksi-Dreieck mit der Turtlegraphik
 
 import turtle as t
 
@@ -7,22 +7,22 @@ l = 400
 wn = t.Screen()
 wn.bgcolor("black")
 wn.setup(width = 600, height = 400)
-wn.title("Sierpinsky-Dreieck")
+wn.title("Sierpinski-Dreieck")
 
 sierp = t.Turtle()
 sierp.pencolor("white")
 sierp.pensize(2)
 
-def sierpinsky(l):
+def sierpinski(l):
     if l > 20:
         for i in range(3):
-            sierpinsky(l/2)
+            sierpinski(l/2)
             sierp.forward(l)
             sierp.right(120)
 
 sierp.penup()
 sierp.goto(-200, 180)
 sierp.pendown()
-sierpinsky(l)
+sierpinski(l)
 
 wn.mainloop()
