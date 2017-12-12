6ee75495e5e41ea9b19b54e0d5f7ea82709f009c
Jörg Kantel <joerg@kantel.de>
2017-12-12 17:48:17 +0100

    Hexagon Random Walk

---
+++ sources/turtle/hexagon.py
@@ -0,0 +1,27 @@
+import turtle as t
+
+wn = t.Screen()
+wn.colormode(255)
+wn.bgcolor(43, 62, 80)
+wn.setup(width = 600, height = 600)
+wn.title("Ein Hexagon mit der Schildkröte")
+
+hexi = t.Turtle()
+hexi.pensize(2)
+hexi.pencolor(253, 141, 60)
+
+hexi.seth(0)
+hexi.fd(100)
+hexi.seth(60)
+hexi.fd(100)
+hexi.seth(120)
+hexi.fd(100)
+hexi.seth(180)
+hexi.fd(100)
+hexi.seth(240)
+hexi.fd(100)
+hexi.seth(300)
+hexi.fd(100)
+
+
+wn.mainloop()
